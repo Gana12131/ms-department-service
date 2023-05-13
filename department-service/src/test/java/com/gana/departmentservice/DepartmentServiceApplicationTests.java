f821a3b9a30a6ac5f4ff017c0cbe73ac7aa130b2
Gana <ganapathi12131@gmail.com>
2023-05-13 09:22:38 +0530

    Initial commit

---
+++ department-service/src/test/java/com/gana/departmentservice/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gana.departmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
